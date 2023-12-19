cd8c9ddd1d959074faef42414cccb1b1aceeda13
Ángel Pereira <ampr2003@gmail.com>
2023-12-19 12:42:09 +0100

    Recuperar Contraseña v11

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -122,7 +122,7 @@ export class AuthService {
     
     // await this.sendEmail(updateAuthDto.email, "Nuevas credenciales del registro en FrutyFest", `<h3>Bienvenid@ ${updateAuthDto.name}, tus nuevas credenciales son las siguientes:</h3>\n\n<p>Usuario: ${updateAuthDto.email}</p>\n<p>Contraseña: ${updateAuthDto.password}</p>`);
     
-    updateAuthDto.password = bcryptjs.hashSync(updateAuthDto.password, 10);
+    // updateAuthDto.password = bcryptjs.hashSync(updateAuthDto.password, 10);
 
     this.userModel.updateOne(restBefore, updateAuthDto);
 
