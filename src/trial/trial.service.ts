22334ee8414b10d4730b0d6da4af5aa76e6bdffe
Ángel Pereira <ampr2003@gmail.com>
2024-01-16 17:32:29 +0100

    Actualizar Trial

--- src/trial/trial.service.ts
+++ src/trial/trial.service.ts
@@ -1,50 +1,60 @@
-import { BadRequestException, Injectable, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { CreateTrialDto } from './dto';
 import { UpdateTrialDto } from './dto';
 import { Trial } from './entities/trial.entity';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
-
 @Injectable()
 export class TrialService {
+  constructor(
+    @InjectModel(Trial.name)
+    private trialModel: Model<Trial>,
+  ) {}
 
-    constructor(
-        @InjectModel(Trial.name)
-        private trialModel: Model<Trial>,) {
+  async create(createTrialDto: CreateTrialDto) {
+    try {
+      const newTrial = new this.trialModel({
+        ...createTrialDto,
+      });
+
+      return await newTrial.save();
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(`${error} already exists!`);
+      }
+      throw new InternalServerErrorException('Something terrible happen!!!');
     }
+  }
 
-    async create(createTrialDto: CreateTrialDto) {
-        try {
-            const newTrial = new this.trialModel({
-                ...createTrialDto
-            }
-            );
+  findAll(): Promise<Trial[]> {
+    return this.trialModel.find();
+  }
 
-            return await newTrial.save();
-        } catch (error) {
-            if (error.code === 11000) {
-                throw new BadRequestException(`${error} already exists!`)
-            }
-            throw new InternalServerErrorException('Something terrible happen!!!')
-        }
-    }
+  async findTrialById(id: string) {
+    return await this.trialModel.findById(id);
+  }
 
-    findAll(): Promise<Trial[]> {
-        return this.trialModel.find();
-    }
+  async update(id: string, updateTrialDto: UpdateTrialDto) {
+    const trial = await this.trialModel.findById(id);
+    const { ...trialData } = updateTrialDto;
 
-    async findTrialById(id: string) {
-        return await this.trialModel.findById(id);
-    }
+    const changeTrial = trial;
 
-    async update(id: string, updateTrialDto: UpdateTrialDto) {
-        const Trial = this.findTrialById(id);
-        return this.trialModel.updateOne(Trial, updateTrialDto);
-    }
+    changeTrial.rated = trialData.rated;
 
-    async remove(id: string) {
-        const Trial = (await this.findTrialById(id)).toJSON();
-        return this.trialModel.deleteOne(Trial);
-    }
+    changeTrial.save();
+
+    return changeTrial.toJSON();
+  }
+
+  async remove(id: string) {
+    const Trial = (await this.findTrialById(id)).toJSON();
+    return this.trialModel.deleteOne(Trial);
+  }
 }
