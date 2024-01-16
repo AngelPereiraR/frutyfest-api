9f53e701fe954c8de6a8bdaa64ee5a1f3b013d74
Ángel Pereira <ampr2003@gmail.com>
2024-01-16 12:29:11 +0100

    Arreglo de problema de CORS v0.7

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    origin: 'http://localhost:4200',
-    credentials: true,
-  });
+  app.enableCors();
 
   app.useGlobalPipes(
     new ValidationPipe({
