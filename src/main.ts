31dca24215802cb5acb694e2feee1c9986c0769c
Ángel Pereira <ampr2003@gmail.com>
2024-01-16 12:27:17 +0100

    Arreglo de problema de CORS v0.6

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    allowedHeaders: ['Content-Type'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     origin: 'http://localhost:4200',
     credentials: true,
   });
