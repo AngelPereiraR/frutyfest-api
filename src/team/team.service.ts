c08fee4d19ad81d2b3f30f21c532172fb601b622
Ángel Pereira <ampr2003@gmail.com>
2024-01-16 12:58:41 +0100

    Actualizar Team v0.4

--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -43,7 +43,7 @@ export class TeamService {
             const Team = this.findTeamById(id);
         return this.teamModel.updateOne(Team, updateTeamDto);
         } catch( error ) {
-            throw new Error(error);
+            throw new Error(error.code);
         }
     }
 
